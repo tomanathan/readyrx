32c903a6c4f7af61882634175731f8b2639c4a01
tomanathan <68439997+tomanathan@users.noreply.github.com>
2024-09-08 20:58:00 -0500

    fix thing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import logoImage from '/Users/mackbook/Documents/Pharmapp/readyrx/src/assets/images/2024-08-23.jpg';
+import logoImage from '/Users/mackbook/Documents/Pharmapp/readyrx/src/2024-08-23.jpg';
 
 const NavbarContainer = styled.nav`
   position: sticky;
